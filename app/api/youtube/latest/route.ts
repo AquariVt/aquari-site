4ed54f490c1cdc7cec46863cfd9fe93d919442d5
Shoya Suzuki <shm02ev20@gmai.com>
2026-04-16 23:06:49 +0900

    fix env usage

--- app/api/youtube/latest/route.ts
+++ app/api/youtube/latest/route.ts
@@ -1,8 +1,7 @@
 import { NextResponse } from "next/server";
 
-const API_KEY = process.env.YOUTUBE_API_KEY;AIzaSyCEGKBap7YoNFZcPwGnOPOBjaPGsKtx6jk
-const CHANNEL_ID = process.env.YOUTUBE_CHANNEL_ID;UCBvSY3MYEkkJ194_Zdjp2Jw
-
+const API_KEY = process.env.YOUTUBE_API_KEY=あなたのAPIキー
+const CHANNEL_ID = process.env.YOUTUBE_CHANNEL_ID=UCBvSY3MYEkkJ194_Zdjp2Jw
 export async function GET() {
   try {
     if (!API_KEY) {
